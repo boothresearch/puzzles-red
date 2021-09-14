81c1c773406477a4918feac3b85ff49da65f255b
tobi <Tobias.Huelden@chicagobooth.edu>
2021-09-14 10:23:50 -0500

    Added True/False return to leap function.

--- leap/leap.py
+++ leap/leap.py
@@ -2,5 +2,7 @@ def leap_year(year):
     
     if (year%4==0)&(year%100!=0)&(year%400!=0):
         print('The year', year, 'is a leap year')
+        return(True)
     else:
         print('The year', year, 'is not a leap year')
+        return(False)
