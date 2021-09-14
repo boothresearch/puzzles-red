470de03bf34be0537630d6d65d621bf577858ec2
Marek Bojko <marek.bojko@chicagobooth.edu>
2021-09-14 12:02:33 -0500

    matrix
    Haven't figured it yet

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,18 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
+        self.rows_list = self.rows()
+
+    def rows(self):
+        if '\n' not in self.matrix_string:
+            return [int(self.matrix_string)]
+        else:
+            rows_str = self.matrix_string.split('\n ')
+            rows_l = [r.split(' ') for r in rows_str]
+            return rows_l
 
     def row(self, index):
-        pass
+        return self.rows_list[index-1]
 
     def column(self, index):
-        pass
+        return [self.rows_list[i][index-1] for i in range(self.rows_list[0])]
