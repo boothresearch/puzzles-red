81c2a3f01a57a6354e769327d2457d4100fe731a
tobi <Tobias.Huelden@chicagobooth.edu>
2021-09-14 10:52:28 -0500

    Fixed leap function. Passes tests now.

--- leap/leap.py
+++ leap/leap.py
@@ -1,8 +1,16 @@
 def leap_year(year):
     
-    if (year%4==0)&(year%100!=0)&(year%400!=0):
-        print('The year', year, 'is a leap year')
-        return(True)
+    if (year%4==0):
+        if (year%100==0):
+            if (year%400==0):
+                print('The year', year, 'is a leap year')
+                return(True)
+            else:
+                print('The year', year, 'is not a leap year')
+                return(False)
+        else:
+            print('The year', year, 'is not a leap year')
+            return(True)
     else:
         print('The year', year, 'is not a leap year')
         return(False)
