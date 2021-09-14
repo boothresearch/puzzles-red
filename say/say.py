c4a688b364b91c5041a724ebcc28cb7160563c4a
tobi <Tobias.Huelden@chicagobooth.edu>
2021-09-14 17:57:29 -0500

    Say passes the tests.

--- say/say.py
+++ say/say.py
@@ -25,8 +25,8 @@ def say(number):
         dict_multiples_ten = {
             2:'twenty',
             3:'thirty',
-            4:'fourthy',
-            5:'fivty',
+            4:'forty',
+            5:'fifty',
             6:'sixty',
             7:'seventy',
             8:'eighty',
@@ -47,8 +47,7 @@ def say(number):
         elif number>99:
             first_digit = int(number/100)
             second_digit = number - first_digit*100
-            print('second digit :', second_digit)
-            words = dict_one_ten[first_digit] + ' hundred ' + return_string(second_digit)
+            words = dict_one_ten[first_digit] + ' hundred and ' + return_string(second_digit)
         return(words)
 
     def split_number(number):
@@ -62,6 +61,16 @@ def say(number):
         return(result)
 
     def add_scaling(chunks):
+        if 'hundred' not in chunks[-1]:
+            chunks[-1] = 'and ' + chunks[-1]
+        if ('and zero' in chunks[-1]):
+            chunks[-1] = str(chunks[-1])
+            chunks[-1] = chunks[-1].replace('and zero', '')
+        if ('and' in chunks[-1]) & (len(chunks)==1) & ('hundred' not in chunks[-1]):
+            chunks[-1] = str(chunks[-1])
+            chunks[-1] = chunks[-1].replace('and', '')
+
+
         if len(chunks)==1: 
             return chunks
         if len(chunks)==2:
@@ -74,12 +83,23 @@ def say(number):
             return  chunks[0] + ' trillion '+ chunks[1] + ' billion ' + chunks[2] + 'million' + chunks[3] + ' thousand ' + chunks[4]
   
     if (number>999999999999)|(number<0):
-         print('Number not in correct range')
-         return False
+         raise ValueError('Number not in correct range')
+
     else:
+        #Perform basic calculations
         chunks = split_number(number)
-        print('Chunks ', chunks)
         chunks = [return_string(int(chunk)) for chunk in chunks]
         chunks = add_scaling(chunks)
-        print('The word is: ', chunks)
-        return(chunks)
+
+
+        #Treat edge cases
+        number_string = ''.join(chunks).strip()
+        number_string = number_string.replace(' and zero thousand', ' thousand')
+        number_string = number_string.replace(' and zero million', ' million')
+        number_string = number_string.replace(' and zero billion', ' billion')
+        number_string = number_string.replace(' zero thousand', '')
+        number_string = number_string.replace(' zero million', '')
+        if number_string=='':
+            number_string='zero'
+
+        return(number_string)
