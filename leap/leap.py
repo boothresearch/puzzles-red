4bb777af9ef25066d37f13bf981a5ae5a52ef99a
tobi <Tobias.Huelden@chicagobooth.edu>
2021-09-14 10:19:07 -0500

    First draft for leap puzzle

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,6 @@
 def leap_year(year):
-    pass
+    
+    if (year%4==0)&(year%100!=0)&(year%400!=0):
+        print('The year', year, 'is a leap year')
+    else:
+        print('The year', year, 'is not a leap year')
